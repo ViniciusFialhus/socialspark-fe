780e170ceb20532ad569a7664989ad85968c8e9d
ViniciusFialhus <contatoviniciusfialhu@gmail.com>
2024-05-09 17:01:05 -0300

    feat: commit de alicere

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.containerMain}>
+      <div className={styles.containerTopArea}></div>
+    </main>
+  );
+}
