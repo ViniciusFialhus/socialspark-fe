d9993d61c22b6ec7d7cd2dd438d6a1abfb2df86c
ViniciusFialhus <contatoviniciusfialhu@gmail.com>
2024-05-24 23:53:41 -0300

    feat: ajuste cors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
   useEffect(() => {
     const clientId = typeof window !== "undefined" ? localStorage.getItem("clientID") : null;
     const letterName = typeof window !== "undefined" ? localStorage.getItem("letterName") : null;
-    const socket = io("https://curved-lemur-social12313-7f7b4f8b.koyeb.app", {
+    const socket = io("http://localhost:3000", {
       withCredentials: true,
       reconnection: false,
       query: { clientId, letterName },
